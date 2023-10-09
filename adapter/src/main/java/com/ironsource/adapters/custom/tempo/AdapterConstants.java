38f0180c3a395122001cc8a77ed94f16b16c9b86
omnibaker <37693234+omnibaker@users.noreply.github.com>
2023-10-09 13:16:38 +1100

    sdk manifest updated

--- adapter/src/main/java/com/ironsource/adapters/custom/tempo/AdapterConstants.java
+++ adapter/src/main/java/com/ironsource/adapters/custom/tempo/AdapterConstants.java
@@ -1,7 +1,7 @@
 package com.ironsource.adapters.custom.tempo;
 
 public class AdapterConstants {
-    public static final String ADAPTER_VERSION = "1.3.0";
+    public static final String ADAPTER_VERSION = "1.3.1";
     public static final String ADAPTER_TYPE = "IRONSOURCE";
     public static final String PARAM_CPM_FLR = "cpmFloor";
     public static final String PARAM_APP_ID = "appId";
