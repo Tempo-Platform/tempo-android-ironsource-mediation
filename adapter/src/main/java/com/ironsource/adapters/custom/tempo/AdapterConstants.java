9f89a249bba304a00109ed1e0279a9ecdd31b97f
omnibaker <37693234+omnibaker@users.noreply.github.com>
2024-11-26 12:04:06 +1100

    rc-1

--- adapter/src/main/java/com/ironsource/adapters/custom/tempo/AdapterConstants.java
+++ adapter/src/main/java/com/ironsource/adapters/custom/tempo/AdapterConstants.java
@@ -1,7 +1,7 @@
 package com.ironsource.adapters.custom.tempo;
 
 public class AdapterConstants {
-    public static final String ADAPTER_VERSION = "1.6.3";
+    public static final String ADAPTER_VERSION = "1.7.0-rc.1";
     public static final String ADAPTER_TYPE = "IRONSOURCE";
     public static final String PARAM_CPM_FLR = "cpmFloor";
     public static final String PARAM_APP_ID = "appId";
