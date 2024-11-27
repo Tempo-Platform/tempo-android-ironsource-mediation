8d6962d742880053881e7e536179a86298361f08
omnibaker <37693234+omnibaker@users.noreply.github.com>
2024-11-27 13:13:15 +1100

    rc7 compileTime

--- adapter/src/main/java/com/ironsource/adapters/custom/tempo/AdapterConstants.java
+++ adapter/src/main/java/com/ironsource/adapters/custom/tempo/AdapterConstants.java
@@ -1,7 +1,7 @@
 package com.ironsource.adapters.custom.tempo;
 
 public class AdapterConstants {
-    public static final String ADAPTER_VERSION = "1.7.0-rc.5";
+    public static final String ADAPTER_VERSION = "1.7.0-rc.7";
     public static final String ADAPTER_TYPE = "IRONSOURCE";
     public static final String PARAM_CPM_FLR = "cpmFloor";
     public static final String PARAM_APP_ID = "appId";
